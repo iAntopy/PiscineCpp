5c8ab49e14d9614755b8e1c10ec0d64cfd0be056
Ian Antony Mongeon Charest <i-antony@hotmail.com>
2023-02-16 22:36:33 -0500

    m00e02 complet, Accounting logs are perfect. All good

--- Module00/ex02/Account.cpp
+++ Module00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: iamongeo <iamongeo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 18:43:47 by iamongeo          #+#    #+#             */
-/*   Updated: 2023/02/15 23:51:06 by iamongeo         ###   ########.fr       */
+/*   Updated: 2023/02/16 22:30:02 by iamongeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,7 @@ int	Account::_totalAmount = 0;
 int	Account::_totalNbDeposits = 0;
 int	Account::_totalNbWithdrawals = 0;
 
+
 void	gen_timestamp(std::string& ret)
 {
 	std::stringstream	timestamp;
@@ -31,51 +32,151 @@ void	gen_timestamp(std::string& ret)
 	ret = timestamp.str();
 }
 
-int	log_account_creation(int index, int amount)
+static bool	open_log_file(std::ofstream& file, std::string& timestamp)
+{
+	std::string		filename;
+	
+//	filename = "logs/";
+	filename += timestamp;
+	filename += ".log";
+	file.open(filename, std::ios_base::app);
+	if (!file)
+	{
+		std::cerr << "Log file open failed" << std::endl;
+		return (false);
+	}
+	file << '[' << timestamp << "] ";
+	return (true);
+}
+
+static int	log_account_creation(int index, int amount)
 {
 	std::string		timestamp;
 	std::ofstream	file;
-	std::string		filename;
 
-	timestamp.clear();
 	gen_timestamp(timestamp);
-	std::cout << "Account created at : " << timestamp << std::endl;
-	filename = timestamp;
-	filename += ".log";
-	file.open(filename, std::ios_base::app);
-	file << '[' << timestamp << "] ";
-	file << "index:" << index;
-	file << "amount:" << amount;
-	file << "created";	
-//	filename.clear();
-	//file.open();
-	file.close();
+	if (open_log_file(file, timestamp))
+	{
+		std::cout << "Account created at : " << timestamp << std::endl;
+		file << "index:" << index;
+		file << ";amount:" << amount;
+		file << ";created" << std::endl;	
+		file.close();
+	}
 	return (0);
 }
 
-Account::Account(int initial_deposit) : _amount(initial_deposit)
+static int	log_account_destruction(int index, int amount)
+{
+	std::string		timestamp;
+	std::ofstream	file;
+
+	gen_timestamp(timestamp);
+	if (open_log_file(file, timestamp))
+	{
+		std::cout << "Account closed at : " << timestamp << std::endl;
+		file << "index:" << index;
+		file << ";amount:" << amount;
+		file << ";closed" << std::endl;	
+		file.close();
+	}
+	return (0);
+}
+
+static int	log_AccountsInfos(int nbAccounts, int amount, int nbDeps, int nbWit)
+{
+	std::string		timestamp;
+	std::ofstream	file;
+
+	gen_timestamp(timestamp);
+	if (open_log_file(file, timestamp))
+	{
+		file << "accounts:" << nbAccounts;
+		file << ";total:" << amount;
+		file << ";deposits:" << nbDeps;	
+		file << ";withdrawals:" << nbWit << std::endl;
+		file.close();
+	}
+	return (0);
+}
+
+static int	log_deposit(int index, int amount, int depAmount, int nbDeps)
+{
+	std::string		timestamp;
+	std::ofstream	file;
+
+	gen_timestamp(timestamp);
+	if (open_log_file(file, timestamp))
+	{
+		file << "index:" << index;
+		file << ";p_amount:" << amount - depAmount;
+		file << ";deposit:" << depAmount;
+		file << ";amount:" << amount;
+		file << ";nb_deposits:" << nbDeps << std::endl;
+		file.close();
+	}
+	return (0);
+}
+
+static int	log_withdrawal(int index, int amount, int witAmount, int nbWits)
+{
+	std::string		timestamp;
+	std::ofstream	file;
+
+	gen_timestamp(timestamp);
+	if (open_log_file(file, timestamp))
+	{
+		file << "index:" << index;
+		if (witAmount == -1)
+		{
+			file << ";p_amount:" << amount;
+			file << ";withdrawal:refused" << std::endl;
+		}
+		else
+		{
+			file << ";p_amount:" << amount + witAmount;
+			file << ";withdrawal:" << witAmount;
+			file << ";amount:" << amount;
+			file << ";nb_withdrawals:" << nbWits << std::endl;
+		}
+		file.close();
+	}
+	return (0);
+}
+
+void	Account::displayStatus(void) const
+{
+	std::string		timestamp;
+	std::ofstream	file;
+
+	gen_timestamp(timestamp);
+	if (open_log_file(file, timestamp))
+	{
+		file << "index:" << this->_accountIndex;
+		file << ";amount:" << this->_amount;
+		file << ";deposits:" << this->_nbDeposits;
+		file << ";withdrawals:" << this->_nbWithdrawals << std::endl;
+		file.close();
+	}
+}
+
+void	Account::_displayTimestamp(void)
+{
+	std::string		timestamp;
+	gen_timestamp(timestamp);
+	std::cout << timestamp << std::endl;	
+}
+
+Account::Account(int initial_deposit) : _amount(initial_deposit),
+	_nbDeposits(0), _nbWithdrawals(0)
 {
 	std::string		timestamp;
 	std::ofstream	file;
 	std::string		filename;
 
 	log_account_creation(this->_accountIndex, this->_amount);
-	timestamp.clear();
-	gen_timestamp(timestamp);
-	std::cout << "Account created at : " << timestamp << std::endl;
-	filename = timestamp;
-	filename += ".log";
-	
-	file.open(filename, std::ios_base::app);
-	file << '[' << timestamp << "] ";
-	file << "index:" << this->_accountIndex;
-	file << "amount:" << this->_amount;
-	file << "created";
-	file.close();
-	
 	this->_totalAmount += initial_deposit;
 	this->_accountIndex = this->_nbAccounts++;
-//	[19920104_091532] index:0;amount:42;created
 }
 
 Account::Account(void) : _amount(0)
@@ -84,7 +185,9 @@ Account::Account(void) : _amount(0)
 }
 
 Account::~Account(void)	{
-	std::cout << "Account " << this->_accountIndex << " destroyed" << std::endl;
+//	std::cout << "Account " << this->_accountIndex << " destroyed" << std::endl;
+
+	log_account_destruction(this->_accountIndex, this->_amount);
 	this->_nbAccounts--;
 }
 
@@ -107,8 +210,10 @@ int Account::checkAmount(void) const {
 	return (this->_amount);}
 
 // Static methods
-void    displayAccountsInfos(void)
+void    Account::displayAccountsInfos(void)
 {
+	log_AccountsInfos(Account::getNbAccounts(), Account::getTotalAmount(),
+		Account::getNbDeposits(), Account::getNbWithdrawals());
 }
 
 // Local methods
@@ -117,38 +222,49 @@ void    Account::makeDeposit(int deposit)
 	this->_amount += deposit;
 	this->_totalAmount += deposit;
 	this->_totalNbDeposits++;
-	this->_nbDeposits++;    
+	this->_nbDeposits++;
+	log_deposit(this->_accountIndex, this->_amount, deposit, this->_nbDeposits);
 }
+
 bool	Account::makeWithdrawal(int withdrawal)
 {
 	if (this->_amount < withdrawal)
 	{
 		std::cerr << "[> ACCOUNT OVERDRAFT ALERT <] : ";
 		std::cerr << "Not enough funds to cover withdrawal." << std::endl;
+		log_withdrawal(this->_accountIndex, this->_amount, -1, this->_nbWithdrawals);
 		return (false);
 	}
 	else if (withdrawal < 0)
 	{
 		std::cerr << "[> INVALID OPERATION ALLERT <] : ";
 		std::cerr << "Withdrawal of absurd amount detected : " << withdrawal << std::endl;
+		log_withdrawal(this->_accountIndex, this->_amount, -1, this->_nbWithdrawals);
 		return (false);
 	}
 	else
 	{
+		//int index, int p_amount, int witAmount, int nbWits)
 		this->_amount -= withdrawal;
 		this->_totalAmount -= withdrawal;
 		this->_totalNbWithdrawals++;
 		this->_nbWithdrawals++;
+		log_withdrawal(this->_accountIndex, this->_amount, withdrawal, this->_nbWithdrawals);
 		return (true);
 	}
 }
-
-void	Account::displayStatus(void) const
-{
-}
-
+/*
 int	main(void)
 {
 	Account	acc(42);
+	acc.displayAccountsInfos();
+	acc.displayStatus();
+
+	acc.makeDeposit(999);
+	acc.makeWithdrawal(42);
+	
+	acc.displayAccountsInfos();
+	acc.displayStatus();
 	return (0);
 }
+*/
